e902d55755a6717427f929f06f9147d0b7cdfeb5
A-People <18712728771@163.com>
2016-08-07 16:54:32 +0800

    0807

--- blogs/src/components/NavBar.js
+++ blogs/src/components/NavBar.js
@@ -2,15 +2,36 @@ import React from 'react';
 import { Link } from 'react-router';
 import Drawer from 'material-ui/Drawer';
 import MenuItem from 'material-ui/MenuItem';
+import {hashHistory} from 'react-router'
 
 class NavBar extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {open: false};
+    this.state = {
+      open: false,
+      title:''
+    }
   }
-  handleToggle(){this.setState({open: !this.state.open});}
+  handleToggle(){
+    this.setState({open: !this.state.open})
+  }
 
   handleClose(){this.setState({open: false});}
+
+  setNavState(){
+    this.setState({
+      title:this.context.router.isActive('/', true) ? 'HOME' :
+        this.context.router.isActive('/blog')? 'BLOG' :
+        this.context.router.isActive('/about')? 'ABOUT' : 'HOME'
+    });
+  }
+  componentWillReceiveProps() {
+    this.setNavState()
+  }
+  componentDidMount(){
+    this.setNavState()
+  }
+
   render () {
     let styles={
       root:{
@@ -27,7 +48,8 @@ class NavBar extends React.Component {
         color:'#fff',
         margin:'0',
         padding:'20px 0',
-        textAlign:'center'
+        textAlign:'center',
+        fontSize:'20px'
       },
       list:{
         textAlign:'center'
@@ -46,7 +68,7 @@ class NavBar extends React.Component {
           open={this.state.open}
           onRequestChange={(open) => this.setState({open})}
         >
-          <h2 style={styles.h2}>MY BLOG</h2>
+          <h2 style={styles.h2}>MY BLOG@{this.state.title}</h2>
           <Link to="/" style={styles.link} activeStyle={{color:'red'}} onlyActiveOnIndex={true}><MenuItem onTouchTap={this.handleClose.bind(this)}>首页</MenuItem></Link>
           <Link to="blog" style={styles.link} activeStyle={{color:'red'}}><MenuItem onTouchTap={this.handleClose.bind(this)}>博客</MenuItem></Link>
           <Link to="about" style={styles.link} activeStyle={{color:'red'}}><MenuItem onTouchTap={this.handleClose.bind(this)}>关于</MenuItem></Link>
@@ -56,4 +78,8 @@ class NavBar extends React.Component {
   }
 }
 
-export default NavBar;
+NavBar.contextTypes = {
+  router: React.PropTypes.object.isRequired,
+}
+
+export default NavBar
